e92c48dfc43132e03d1f92853088e081bf4b0195
Maxime Nanouche <maxime.nanouche@viagogo.com>
2015-12-10 11:20:12 +0000

    Add manifest

--- gogokit/config.py
+++ gogokit/config.py
@@ -1,4 +1,4 @@
 __pypi_packagename__ = "gogokit"
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 __token_url__ = "https://account.viagogo.com/oauth2/token"
 __root_url__ = "https://api.viagogo.net/v2"
